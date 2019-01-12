a0e1ca3875b320f6f3ca622ee8d3b4b4d4989783
Lucas Calegari Alves de Oliveira <lcalegari17@gmail.com>
2019-01-12 09:16:20 -0800

    Still working on the challenge

--- server.js
+++ server.js
@@ -4,12 +4,9 @@ var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser')
-var btoa = require('btoa')
-var atob = require('atob')
-const dns = require('dns')
 var cors = require('cors')
 var shortid = require("shortid")
-var AutoIncrement = require('mongoose-sequence')(mongoose);
+var validUrl = require('valid-url')
 
 var app = express();
 
@@ -22,7 +19,6 @@ mongoose.connect(process.env.MONGO_URI);
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
-// you should mount the body-parser here
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -35,12 +31,8 @@ app.get('/', function(req, res){
 var urlSchema = new mongoose.Schema({
   originalUrl: String, // The original URL
   shortUrl: String, // The url shortened
-  counter: { type: Number, default: 0 }, // The counter
   createdAt: { type: Date, default: Date.now }
 });
-
-// Implementing the counter
-urlSchema.plugin(AutoIncrement, {inc_field: 'counter'} );
 
 var URL = mongoose.model('URL', urlSchema);
 
@@ -48,41 +40,37 @@ var URL = mongoose.model('URL', urlSchema);
 app.post("/api/shorturl/new", function(req,res) {
   var url = req.body.url;
   
-  function isValidDnsUrl(url) {
-    dns.lookup(url, function(err, address, family) {
-      console.log( url );
-      console.log( err );
-      if (err) errorObject();
-    });
-  }
+  // Todo : Url is not getting verified properly
+  // Todo : ShortId is not returned immediately in the post request
   
-  if ( isValidDnsUrl(url) ) {
+  if ( validUrl.isUri(url) ) {
     // Looking for a url that was already shortened
     URL.findOne( { originalUrl: url }, function(err, doc) {
        if (doc) {
-          console.log('it means a record was found'); 
           res.json({
-            "original_url" : url,
-            "short_url" : doc.counter
+            "original_url" : doc.originalUrl,
+            "short_url" : doc.shortUrl
          });
        } else {
-          console.log('no record found, so lets create one');
+          var shortId = shortid.generate();
           var newShortenedUrl = new URL({
             originalUrl: url,
-            shortUrl : url + '/' + shortid.generate()
+            shortUrl : shortId
           });
 
           // Saving shortened url
           newShortenedUrl.save();
-          returnObject( { originalUrl: newShortenedUrl.originalUrl, short_url: newShortenedUrl.short_url } );
+          returnObject(newShortenedUrl);
        }
     });  
+  } else {
+     errorObject();
   }
   
-  function returnObject(originalUrl, short_url) {
+  function returnObject(result) {
      res.json({
-       "original_url" : originalUrl,
-       "short_url" : short_url
+       "original_url" : result.originalUrl,
+       "short_url" : result.short_url
      });
   }
   
@@ -94,6 +82,19 @@ app.post("/api/shorturl/new", function(req,res) {
   
 });
 
+// Implement a get route in the /shorurl/ to get the current url shortened
+app.get("/api/shorturl/:shortUrl", function(req,res) {
+  var shortUrl = req.params.shortUrl;
+  
+  URL.findOne( { shortUrl: shortUrl }, function(err, doc) {
+    if (doc) {
+       res.redirect(doc.originalUrl);
+    } else {
+       res.redirect('/');
+    }
+  });
+  
+});
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
